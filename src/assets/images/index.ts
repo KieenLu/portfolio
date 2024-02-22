f8eecab35a7791c429f86ddc70795683767de404
Kiên Lư <kienlu2000@gmail.com>
2024-02-22 16:20:30 +0700

    feat: create project page

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as ImageProjectBenoit } from "./benoit.png";
+export { default as ImageProjectCFDCourse } from "./cfdcourse.png";
+export { default as ImageProjectShopper } from "./shopper.png";
+export { default as ImageProjectWooder } from "./wooder.png";
