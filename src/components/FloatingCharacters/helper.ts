3fa077317293a4b878b53ce139934bdc97fcf13a
kienlu <kienlu2000@gmail.com>
2026-01-13 09:58:20 +0700

    style: update light

--- src/components/FloatingCharacters/helper.ts
+++ src/components/FloatingCharacters/helper.ts
@@ -2,37 +2,37 @@ export const CHARACTERS = [
     {
         char: "{}",
         position: { x: -5.5, y: -2, z: 7 },
-        size: 2,
+        size: 1.5,
         rotation: { x: -0.2, y: 1, z: 0.1 },
     },
     {
         char: "2",
         position: { x: -6.5, y: 3.2, z: 2 },
-        size: 2.0,
+        size: 1.5,
         rotation: { x: -0.2, y: 0.1, z: 0.3 },
     },
     {
         char: "5",
         position: { x: -6.2, y: 4.2, z: -0.5 },
-        size: 1.8,
+        size: 1.3,
         rotation: { x: 0.2, y: -0.1, z: 0.1 },
     },
     {
         char: "1",
         position: { x: 7.8, y: -2, z: 1 },
-        size: 2.2,
+        size: 1.7,
         rotation: { x: -0.1, y: 0.2, z: -0.1 },
     },
     {
         char: "1",
         position: { x: 7.4, y: -3, z: 0.1 },
-        size: 2,
+        size: 1.5,
         rotation: { x: 0.5, y: -0.2, z: -0.2 },
     },
     {
         char: "$",
         position: { x: 7.5, y: 2, z: 6 },
-        size: 5,
+        size: 4.5,
         rotation: { x: 0, y: -0.3, z: 0.1 },
     },
 ];
